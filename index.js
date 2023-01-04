ee7280d64a2023d6ea4e3b94a7288b935d8971f5
Serhii <sergbr78@gmail.com>
2023-01-04 20:26:12 +0100

    work test hw-node-01

---
+++ index.js
@@ -0,0 +1,53 @@
+
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
+// const contacts =  require('./db/contacts.json')
+
+// const PORT = process.env.PORT || 8081;
+
+const {listContacts, getContactById, removeContact, addContact} = require('./contacts');
+
+
+async function invokeAction({ action, id, name, email, phone }) {
+    switch (action) {
+      case "list":
+      const list = await listContacts();
+      console.log(list);
+      // ...
+        break;
+    
+      case "get":
+        const get = await getContactById(id);
+        if(!id){
+            throw new Error (`Contact with ${id} not find`);
+        };
+        console.log(get);
+      // ... id
+        break;
+  
+      case "add":
+        const add = await addContact(name, email, phone);
+
+        console.log(add);
+        // ... name email phone
+        break;
+  
+      case "remove":
+        const remove = await removeContact(id);
+        if(!id){
+            throw new Error (`Contact with ${id} not find`);
+        };
+        console.log(remove);
+        // ... id
+        break;
+  
+      default:
+        console.warn("\x1B[31m Unknown action type!");
+    }
+  }
+  
+  invokeAction(argv);
+
+
+  
